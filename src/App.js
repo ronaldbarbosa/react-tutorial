81314319fe10e0461574e138c6608aa94306cc96
Ronald Barbosa <ronaldverissimo@hotmail.com>
2023-02-08 22:01:12 -0300

    Aula 02: Criacao do React App

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React App</h1>
+      <p>Meu primeiro React App</p>
+    </div>
+  );
+}
+
+export default App;
