7401616b2afff66962764f5ac32b77d66a64d1b0
Ronald Barbosa <ronaldverissimo@hotmail.com>
2023-02-13 23:19:16 -0300

    Aula 10: React hooks: useState

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,28 @@
+import { useState } from "react"
+
 function Form() {
     function cadastrarUsuario(e) {
         e.preventDefault()
-        console.log("Usuário cadastrado")
+        console.log(`${name} - ${password}`)
     }
+
+    const [name, setName] = useState('Ronald') // useState com valor default
+    const [password, setPassword] = useState()
 
     return (
         <>
             <h2>Cadastro:</h2>
             <form onSubmit={cadastrarUsuario}>
                 <div>
-                    <input type="text" placeholder="Digite seu nome" />
+                    <label htmlFor="name">Nome: </label>
+                    <input type="text" id="name" name="name" placeholder="Digite seu nome" 
+                        value={name} // passando o valor default do useState no HTML 
+                        onChange={(e) => setName(e.target.value)} />
+                </div>
+                <div>
+                    <label htmlFor="password">Senha: </label>
+                    <input type="password" id="password" name="password" placeholder="Digite sua senha"
+                        onChange={(e) => setPassword(e.target.value)} />
                 </div>
                 <div>
                     <input type="submit" value="Enviar" />
