70b7757657a9538c5567d84b2cb8a4e52e19a916
Ronald Barbosa <ronaldverissimo@hotmail.com>
2023-02-14 23:09:33 -0300

    Aula 11: passando eventos por props

---
+++ src/components/Button.js
@@ -0,0 +1,7 @@
+// Componente filho de Evento.js
+
+function Button(props) {
+    return <button onClick={props.event}>{ props.text }</button>
+}
+
+export default Button
--- src/components/Evento.js
+++ src/components/Evento.js
@@ -1,12 +1,26 @@
+import Button from "./Button"
+
 function Evento({ numero }) {
     function meuEvento() {
         console.log(`OnClick ativou evento ${numero}`)
+    }
+
+    function outroEvento() {
+        console.log("Segundo evento ativado")
+    }
+
+    function maisUmEvento() {
+        console.log("Terceiro evento ativado")
     }
     
     return (
         <>
             <p>Clique para disparar um evento:</p>
             <button onClick={meuEvento}>Ativar</button>
+            <br />
+            <Button event={outroEvento} text="Segundo evento" />
+            <br />
+            <Button event={maisUmEvento} text="Terceiro evento" />
         </>
     )
 }
