8285e8e5470853dfc3e109bdd13cb8e5795feb30
Ronald Barbosa <ronaldverissimo@hotmail.com>
2023-02-18 22:35:59 -0300

    Aula 16: React Icons

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,11 +1,18 @@
 import { Link } from "react-router-dom"
 import styles from "./Footer.module.css"
 
+import { FaGithub, FaInstagram, FaLinkedin} from 'react-icons/fa'
+
 export default function Footer() {
     return (
         <footer>
-            <p>Ronald Barbosa</p>
+            <p>Ronald Veríssimo Barbosa</p>
+            <FaGithub className={ styles.icon }/>
             <Link to="https://github.com/ronaldbarbosa" className={ styles.link } target="_blank">GitHub</Link>
+            <FaLinkedin className={ styles.icon }/>
+            <Link to="https://www.linkedin.com/in/ronald-barbosa/" className={ styles.link } target="_blank">Linkedin</Link>
+            <FaInstagram className={ styles.icon }/>
+            <Link to="https://instagram.com/ronald_barbosa204" className={ styles.link } target="_blank">Instagram</Link>
         </footer>
     )
 }
